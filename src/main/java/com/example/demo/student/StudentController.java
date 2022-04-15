e65c9178a69ecb79c94b942235da955df3ac8521
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2022-04-15 02:53:23 -0700

    Added GET POST PUT and DELETE method

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,10 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -27,7 +24,20 @@ public class StudentController {
     }
 
     @PostMapping
-    public void registerNewStudent(Student student){
+    public void registerNewStudent(@RequestBody Student student){
         this.studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        this.studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email){
+        this.studentService.updateStudent(studentId, name, email);
+
     }
 }
